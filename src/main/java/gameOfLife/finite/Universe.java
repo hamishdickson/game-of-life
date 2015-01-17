520eb1d08774fe49c9d85b6ecea7ef544c4c01f1
mishyTheBear <hamish.dickson@gmail.com>
2015-01-17 12:50:38 +0000

    small refactoring

--- src/main/java/gameOfLife/finite/Universe.java
+++ src/main/java/gameOfLife/finite/Universe.java
@@ -8,18 +8,18 @@ public class Universe {
     private final int WIDTH;
     private final int HEIGHT;
     
-    public Universe(int xCoord, int yCoord) {
-        this.grid = new boolean[xCoord][yCoord];
-        this.WIDTH = xCoord;
-        this.HEIGHT = yCoord;
+    public Universe(int width, int height) {
+        this.grid = new boolean[width][height];
+        this.WIDTH = width;
+        this.HEIGHT = height;
     }
 
     public Universe iterate() {
         Universe newUniverse = new Universe(this.WIDTH, this.HEIGHT);
-        for (int i = 0; i < HEIGHT; i++) {
-            for (int j = 0; j < WIDTH; j++) {
-                int numberOfNeighbours = this.countNeighbours(j, i);
-                if (numberOfNeighbours == 2 || numberOfNeighbours == 3) newUniverse.setAlive(j, i);
+        for (int j = 0; j < HEIGHT; j++) {
+            for (int i = 0; i < WIDTH; i++) {
+                int numberOfNeighbours = this.countNeighbours(i, j);
+                if (numberOfNeighbours == 2 || numberOfNeighbours == 3) newUniverse.setAlive(i, j);
             }
         }
         return newUniverse;
@@ -85,15 +85,15 @@ public class Universe {
         String DEAD_CELL = " ";
         String ALIVE_CELL = "+";
         
-        for (int i = 0; i < HEIGHT; i++) {
-            for (int j = 0; j < WIDTH; j++) {
-                if (isCellAlive(j, i)) {
+        for (int j = 0; j < HEIGHT; j++) {
+            for (int i = 0; i < WIDTH; i++) {
+                if (isCellAlive(i, j)) {
                     sb.append(ALIVE_CELL);
                 } else {
                     sb.append(DEAD_CELL);
                 }
                 
-                if (j == HEIGHT - 1) sb.append("\n");
+                if (i == HEIGHT - 1) sb.append("\n");
             }
         }
         
