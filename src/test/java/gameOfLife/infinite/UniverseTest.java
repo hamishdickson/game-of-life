644031b079096a675facc538ca00ac412ebc1deb
mishyTheBear <hamish.dickson@gmail.com>
2015-01-29 08:53:53 +0000

    add new tests

--- src/test/java/gameOfLife/infinite/UniverseTest.java
+++ src/test/java/gameOfLife/infinite/UniverseTest.java
@@ -37,9 +37,19 @@ public class UniverseTest extends TestCase {
         
         assertEquals(0, nextUniverse.numberOfAliveCells());
     }
+
+    @Test
+    public void testDeadCellWithNoNeighboursIsNotBorn() {
+        Universe universe = new Universe();
+        CellPosition cellPosition = new CellPosition(1, 1);
+
+        Universe nextUniverse = universe.step();
+
+        assertEquals(0, nextUniverse.numberOfAliveCells());
+    }
     
     @Test
-    public void testCellWithTwoLiveNeighboursLives() {
+    public void testAliveCellWithTwoLiveNeighboursLives() {
         Universe universe = new Universe();
         CellPosition cellPosition1 = new CellPosition(1, 1);
         CellPosition cellPosition2 = new CellPosition(2, 1);
@@ -56,6 +66,55 @@ public class UniverseTest extends TestCase {
         Universe universe1 = universe.step();
         
         assertEquals(true, universe1.cellIsAliveAtPosition(cellPosition3));
+    }
+
+    @Test
+    public void testAliveCellWithThreeLiveNeighboursLives() {
+        Universe universe = new Universe();
+        CellPosition cellPosition1 = new CellPosition(1, 1);
+        CellPosition cellPosition2 = new CellPosition(2, 1);
+        CellPosition cellPosition3 = new CellPosition(3, 1);
+        CellPosition testCellPosition = new CellPosition(2, 2);
+
+        Cell otherCell1 = new AliveCell(cellPosition1);
+        Cell otherCell2 = new AliveCell(cellPosition2);
+        Cell otherCell3 = new AliveCell(cellPosition3);
+        Cell testCell = new AliveCell(testCellPosition);
+
+        universe.addAliveCell(otherCell1);
+        universe.addAliveCell(otherCell2);
+        universe.addAliveCell(otherCell3);
+        universe.addAliveCell(testCell);
+
+        Universe universe1 = universe.step();
+
+        assertEquals(true, universe1.cellIsAliveAtPosition(testCellPosition));
+    }
+
+    @Test
+    public void testAliveCellWithFourLiveNeighboursDies() {
+        Universe universe = new Universe();
+        CellPosition cellPosition1 = new CellPosition(1, 1);
+        CellPosition cellPosition2 = new CellPosition(2, 1);
+        CellPosition cellPosition3 = new CellPosition(3, 1);
+        CellPosition cellPosition4 = new CellPosition(1, 2);
+        CellPosition testCellPosition = new CellPosition(2, 2);
+
+        Cell otherCell1 = new AliveCell(cellPosition1);
+        Cell otherCell2 = new AliveCell(cellPosition2);
+        Cell otherCell3 = new AliveCell(cellPosition3);
+        Cell otherCell4 = new AliveCell(cellPosition4);
+        Cell testCell = new AliveCell(testCellPosition);
+
+        universe.addAliveCell(otherCell1);
+        universe.addAliveCell(otherCell2);
+        universe.addAliveCell(otherCell3);
+        universe.addAliveCell(otherCell4);
+        universe.addAliveCell(testCell);
+
+        Universe universe1 = universe.step();
+
+        assertEquals(false, universe1.cellIsAliveAtPosition(testCellPosition));
     }
 
     @Test
